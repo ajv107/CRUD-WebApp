4440179a3b00f36715408aa9b454816bebde8a56
ajv107 <ajv107@gmail.com>
2022-06-01 10:45:06 -0400

    message

---
+++ CRUD-WebApp/src/main/java/partum/CRUD/WebApp/CrudWebAppApplication.java
@@ -0,0 +1,14 @@
+package partum.CRUD.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWebAppApplication.class, args);
+	}
+
+}
---
+++ CRUD-WebApp/src/main/java/partum/CRUD/WebApp/user/UserRepository.java
@@ -0,0 +1,7 @@
+package partum.CRUD.WebApp.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Integer> {
+
+}
